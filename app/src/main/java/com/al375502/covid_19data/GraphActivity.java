df4a13da52d878a2b3f2c2911c4525417098e8cb
allstendres98 <allstendres@users.noreply.github.com>
2020-05-04 19:52:12 +0200

    spinners

--- app/src/main/java/com/al375502/covid_19data/GraphActivity.java
+++ app/src/main/java/com/al375502/covid_19data/GraphActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CheckBox;
 import android.widget.Spinner;
 
 import com.github.mikephil.charting.charts.BarChart;
@@ -12,12 +13,14 @@ public class GraphActivity extends AppCompatActivity {
     public static final String COUNTRY = "Country";
     BarChart barChart;
     Spinner spinner;
+    CheckBox deaths, confirmed, recovered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
         barChart = findViewById(R.id.bargraph);
+        spinner = findViewById(R.id.yearspinner);
 
 
         Intent intent = getIntent();
